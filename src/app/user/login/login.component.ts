8f3bfd94c75896b3543cdd981e82043469db7cb2
BrianKawinzi <brian.mutisya@starthmore.edu>
2023-07-16 09:12:09 +0300

    login complete

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  //Add any logic or functionality for the login page here
+  user = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    //simulate a simple login process
+    if (this.user.email === 'test@example.com' && this.user.password === 'password') {
+      //Successful login
+      alert('Login successful');
+      this.router.navigate(['/aboutus']);
+    } else {
+      //Failed login
+      alert('Invalid credentials. Please try again.');
+    }
+  }
+
+  onSignup() {
+    this.router.navigate(['/signup']);
+  }
 
 }
